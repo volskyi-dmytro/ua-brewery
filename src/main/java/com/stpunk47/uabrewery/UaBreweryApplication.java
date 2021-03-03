9d35e14e64d5c74a2d9524f931e8f73e1aa5584c
volskyi.dmytro <volskyi.dmytro@gmail.com>
2021-03-03 15:25:18 +0200

    Initial commit

---
+++ src/main/java/com/stpunk47/uabrewery/UaBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.stpunk47.uabrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stpunk47/uabrewery/UaBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpunk47.uabrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
